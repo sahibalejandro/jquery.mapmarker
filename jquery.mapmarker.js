1cbda3d6a0a1c588345ac79986e07f4acaeb72e8
Sahib J. Leo <sahib.alejandro@gmail.com>
2014-06-23 22:34:01 -0500

    Solved issue #5
    Search implemented.
    Some code fixes and improvements.
    API Key changed.

--- jquery.mapmarker.js
+++ jquery.mapmarker.js
@@ -1,3 +1,4 @@
+
 /**
  * jQuery Map Marker v1.0.0
  * @author Sahib J. Leo (sahib.alejandro@gmail.com)
@@ -14,13 +15,36 @@
 		'latField': null,
 		'lngField': null,
 		'onChange': function (location) {},
+		'onSearchStart': function () {console.log('onSearchStart');},
+		'onSearchEnd': function (status) {console.log('onSearchEnd');},
+		'noResultsMsg': 'No results.',
+		/**
+		 * Map options.
+		 * @see https://developers.google.com/maps/documentation/javascript/reference?hl=es#MapOptions
+		 * @type {Object}
+		 */
 		'mapOptions': {
 			'mapTypeId': google.maps.MapTypeId.ROADMAP,
 			'zoom': 12
 		},
+		/**
+		 * Marker options.
+		 * @see https://developers.google.com/maps/documentation/javascript/reference?hl=es#MarkerOptions
+		 * @type {Object}
+		 */
 		'markerOptions': {
 			'draggable': true
-		}
+		},
+		/**
+		 * Selector of element where the search results are displayed.
+		 * @type {String}
+		 */
+		'resultsView': null,
+		/**
+		 * HTML template for each search result.
+		 * @type {String}
+		 */
+		'resultTemplate': '<a href="#">:address</a>'
 	};
 
 	/**
@@ -40,7 +64,7 @@
 			// Extended options
 			var options = $.extend({}, defaultOptions, opts);
 
-			// Element's data, to use with $().data();
+			// Element's data, to use with $().data('mapMarker');
 			var data = {
 				'map': null,
 				'marker': null,
@@ -64,11 +88,11 @@
 			 * Initialize objects
 			 */
 
-			// Initialize objets and store in $().data()
+			// Initialize objets and store in $().data('mapMarker')
 			data.marker   = new google.maps.Marker(options.markerOptions);
 			data.map      = new google.maps.Map(this.get(0), options.mapOptions);
 			data.geocoder = new google.maps.Geocoder();
-			this.data(data);
+			this.data('mapMarker', data);
 
 			// Attach marker into map
 			data.marker.setMap(data.map);
@@ -103,7 +127,7 @@
 		 * @return {jQuery} jQuery object
 		 */
 		'setLocation': function (location) {
-			this.data().marker.setPosition(location);
+			this.data('mapMarker').marker.setPosition(location);
 			this.mapMarker('notifyNewLocation');
 
 			return this;
@@ -116,32 +140,109 @@
 		 */
 		'notifyNewLocation': function () {
 			// Get location from marker
-			var location = this.data().marker.getPosition();
+			var location = this.data('mapMarker').marker.getPosition();
 
 			// Update fields, if they are defined.
-			var latField = this.data().options.latField;
-			var lngField = this.data().options.lngField;
+			var latField = this.data('mapMarker').options.latField;
+			var lngField = this.data('mapMarker').options.lngField;
 			if (latField && lngField) {
 				$(latField).val(location.lat());
 				$(lngField).val(location.lng());
 			}
 
 			// Trigger onChange event.
-			this.data().options.onChange.call(this, location);
+			this.data('mapMarker').options.onChange.call(this, location);
 
 			return this;
 		},
 		// - notifyNewLocation()
+
+		/**
+		 * Perform a search using google.maps.Geocoder
+		 * @param  {String} address Address to search
+		 * @return {jQuery}         jQuery object
+		 */
+		'search': function (address) {
+			// Keep reference for access from within callbacks.
+			var parent = this;
+
+			// Disable map when loading results.
+			this.mapMarker('disable', true);
+
+			// Trigger event "onSearchStart"
+			this.data('mapMarker').options.onSearchStart.call(this);
+
+			// Make request to geocode()
+			this.data('mapMarker').geocoder.geocode({'address': address}, function (results, status) {
+
+				// Enable map
+				parent.mapMarker('disable', false);
+
+				print_results.call(parent, results, status);
+
+				// Trigger event "onSearchEnd"
+				parent.data('mapMarker').options.onSearchEnd.call(this, status);
+			});
+
+			return this;
+		},
+		// - search()
+
+		/**
+		 * Disable/enable map, usefull for async tasks.
+		 * @param  {boolean} disable Disable or enable
+		 * @return {jQuery}          jQuery object
+		 */
+		'disable': function (disable) {
+			console.log('disable map:', disable);
+		},
+		// - disable()
 		
 		/**
 		 * Returns the reference to the google.maps.Map object
 		 * @return {google.maps.Map}
 		 */
 		'getMap': function () {
-			return this.data().map;
+			return this.data('mapMarker').map;
 		}
 		// - getMap()
 	};
+	// - m{}
+
+	/**
+	 * Print geocoder results in resultsView.
+	 */
+	function print_results(results, status) {
+		var parent   = this;
+		var template = this.data('mapMarker').options.resultTemplate;
+		var view     = $(this.data('mapMarker').options.resultsView);
+
+		// Clear old results
+		view.empty();
+
+		switch (status) {
+			case google.maps.GeocoderStatus.OK:
+				// Iterate over results to print each one.
+				$.each(results, function (i, result) {
+					var resultHTML = template.replace(':address', result.formatted_address);
+					var jqResult = $(resultHTML);
+
+					jqResult.data('geocoderResult', result);
+					jqResult.on('click', function (e) {
+						e.preventDefault();
+						var location = $(this).data().geocoderResult.geometry.location;
+						parent.data('mapMarker').map.panTo(location);
+						parent.mapMarker('setLocation', location);
+					});
+
+					view.append(jqResult);
+				});
+				break;
+			default:
+				view.text(status);
+		}
+
+	}
 
 	/* ------------------------------------------------------------------------
 	 * Method calling logic
